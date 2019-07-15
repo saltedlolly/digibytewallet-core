7fa96b04264857e0127bae98f5dac5218c0e1d6c
Yoshi <yoshi@jaeger.berlin>
2019-07-15 13:41:33 +0200

    Yoshis asset mods (2)
    (cherry picked from commit 8445ef218b9721c8eee1fe57f8269087b450fb5c)

--- BRWallet.c
+++ BRWallet.c
@@ -597,7 +597,7 @@ BRTransaction *BRWalletCreateTxForOutputs(BRWallet *wallet, const BRTxOutput out
         tx = BRSetGet(wallet->allTx, o);
 
         if (! tx || o->n >= tx->outCount) continue;
-        if (BROutIsAsset(tx->outputs[o->n])) continue;
+        if (BROutpointIsAsset(&tx->outputs[o->n])) continue;
         BRTransactionAddInput(transaction, tx->txHash, o->n, tx->outputs[o->n].amount,
                               tx->outputs[o->n].script, tx->outputs[o->n].scriptLen, NULL, 0, NULL, 0, TXIN_SEQUENCE);
         
@@ -1281,10 +1281,11 @@ uint8_t BRTXContainsAsset(BRTransaction *tx)
 uint8_t BRContainsAsset(const BRTxOutput *outputs, size_t outCount)
 {
     for (int p = 0; p < outCount; p++)
-        if (BROutIsAsset(&outputs[p])) return 1;
+        if (BROutpointIsAsset(&outputs[p])) return 1;
     return 0;
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 uint8_t BROutIsAsset(const BRTxOutput output) {
     uint8_t one = output.script[0];
@@ -1296,25 +1297,102 @@ uint8_t BROutIsAsset(const BRTxOutput output) {
     return 0;
 }
 =======
+=======
+/* (internal)
+ * Tests the protocol tag of an asset.
+ * Returns 1 if the test was successful, otherwise it returns zero.
+ */
+>>>>>>> 8445ef2... Yoshis asset mods (2)
 uint8_t BRTestProtocolTag(const uint8_t* ptr, const char* check) {
-    if (ptr == NULL || check == NULL) return 0;
+    if (ptr == NULL || check == NULL) return 0;
     return (*ptr == check[0] && *(ptr + 1) == check[1]);
 }
 
+/*
+ * Returns zero if an outpoint is no asset, otherwise
+ * returns the length of the asset's data
+ */
 uint8_t BROutpointIsAsset(const BRTxOutput* output)
-    uint8_t* ptr = output->script;
-    if (output->scriptLen <= 6 || !output->script) return 0;
+{
+    uint8_t* ptr;
+    uint8_t length;
+
+    if (output->scriptLen <= 6 || !output->script) return 0;
+    ptr = output->script;
+
     if (*ptr != OP_RETURN) return 0;
     ++ptr;
 
-    uint8_t length = *ptr;
-    if (length == 0) return 0;
+    length = *ptr;
+    if (length == 0 || length + 2 > output->scriptLen) return 0;
     ++ptr;
 
     if (!BRTestProtocolTag(ptr, "DA")) return 0;
     ++ptr;
 
-    if (*ptr == 0x02) return 0;
+//    if (*ptr == 0x02) return 0;
     return length;
 }
+<<<<<<< HEAD
 >>>>>>> c16bca2... Yoshis asset mods
+=======
+
+/*
+ * Returns 1 if asset was decoded correctly, otherwise zero.
+ */
+uint8_t BRDecodeAsset(const BRTxOutput* output, BRAssetData* data) {
+    uint8_t* ptr;
+    uint8_t length;
+    uint8_t type;
+
+    if (output == NULL || data == NULL) return 0;
+    if (!(length = BROutpointIsAsset(output))) return 0;
+    ptr = output->script + 4; /* OP_RETURN + LEN + TAG */
+    
+    memset(data, 0, sizeof(BRAssetData));
+    
+    data->version = *ptr;
+    ptr++;
+    
+    type = *ptr;
+    
+    if (DA_IS_ISSUANCE(type)) {
+        data->type = DA_ISSUANCE;
+    } else if (DA_IS_TRANSFER(type)) {
+        data->type = DA_TRANSFER;
+    } else if (DA_IS_BURN(type)) {
+        data->type = DA_BURN;
+    }
+    
+    /*
+     * Note that a buffer length check was performed in
+     * BROutpointIsAsset.
+     */
+    if (type & DA_TYPE_SHA1_META_SHA256) {
+        assert(length == 2 + 1 + 20 + 32 && "Invalid length");
+        memcpy(&data->info_hash[0], ptr, 20);
+        memcpy(&data->metadata[0], ptr, 32);
+        data->has_infohash = 1;
+        data->has_metadata = 1;
+        
+    } else if (type & DA_TYPE_SHA1_MS12_SHA256) {
+        
+    } else if (type & DA_TYPE_SHA1_MS13_SHA256) {
+        
+    } else if (type & DA_TYPE_SHA1_META) {
+        assert(length == 2 + 1 + 20 && "Invalid length");
+        memcpy(&data->info_hash[0], ptr, 20);
+        data->has_infohash = 1;
+        
+    } else if (type & DA_TYPE_SHA1_NO_META_LOCKED) {
+        data->locked = 1;
+        
+    } else if (type & DA_TYPE_SHA1_NO_META_UNLOCKED) {
+        data->locked = 0;
+        
+    } else {
+    }
+    
+    return 1;
+}
+>>>>>>> 8445ef2... Yoshis asset mods (2)
