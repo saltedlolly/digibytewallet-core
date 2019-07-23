db39139bfdc2149d228e1788352d47ad3eeabb53
Yoshi <yoshi@jaeger.berlin>
2019-07-23 21:13:32 +0200

    Odo: Some more optimizations

--- crypto/odocrypt.c
+++ crypto/odocrypt.c
@@ -17,10 +17,10 @@ uint64_t Rot(uint64_t x, int r)
     return r == 0 ? x : (x << r) ^ (x >> (64-r));
 }
 
-void OdoCrypt_Unpack(uint64_t* state, const char* bytes)
+void Odocrypt_Unpack(uint64_t* state, const char* bytes)
 {
-    memset(state, 0, STATE_SIZE * sizeof(uint64_t));
-    for (int i = 0; i < STATE_SIZE; i++)
+    memset(state, 0, ODOCRYPT_STATE_SIZE * sizeof(uint64_t));
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
     {
         for (int j = 0; j < 8; j++)
         {
@@ -29,19 +29,19 @@ void OdoCrypt_Unpack(uint64_t* state, const char* bytes)
     }
 }
 
-void OdoCrypt_PreMix(uint64_t* state)
+void Odocrypt_PreMix(uint64_t* state)
 {
     uint64_t total = 0;
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
         total ^= state[i];
     total ^= total >> 32;
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
         state[i] ^= total;
 }
 
-void OdoCrypt_ApplyMaskedSwaps(uint64_t* state, const uint64_t* mask)
+void Odocrypt_ApplyMaskedSwaps(uint64_t* state, const uint64_t* mask)
 {
-    for (int i = 0; i < STATE_SIZE/2; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE/2; i++)
     {
         uint64_t a = state[2*i];
         uint64_t b = state[2*i+1];
@@ -55,19 +55,19 @@ void OdoCrypt_ApplyMaskedSwaps(uint64_t* state, const uint64_t* mask)
     }
 }
 
-void OdoCrypt_ApplyWordShuffle(uint64_t* state, int m)
+void Odocrypt_ApplyWordShuffle(uint64_t* state, int m)
 {
-    uint64_t next[STATE_SIZE];
-    for (int i = 0; i < STATE_SIZE; i++)
+    uint64_t next[ODOCRYPT_STATE_SIZE];
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
     {
-        next[m*i % STATE_SIZE] = state[i];
+        next[m*i % ODOCRYPT_STATE_SIZE] = state[i];
     }
-    memcpy(state, &next[0], STATE_SIZE * sizeof(uint64_t));
+    memcpy(state, &next[0], ODOCRYPT_STATE_SIZE * sizeof(uint64_t));
 }
 
-void OdoCrypt_ApplyPboxRotations(uint64_t* state, const int* rotation)
+void Odocrypt_ApplyPboxRotations(uint64_t* state, const int* rotation)
 {
-    for (int i = 0; i < STATE_SIZE/2; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE/2; i++)
     {
         // Only rotate the even words.  Rotating the odd words wouldn't actually
         // be useful - a transformation that rotates all the words can be
@@ -79,22 +79,22 @@ void OdoCrypt_ApplyPboxRotations(uint64_t* state, const int* rotation)
 
 void OdoCrypt_ApplyPbox(uint64_t* state, const Pbox* perm)
 {
-    for (int i = 0; i < PBOX_SUBROUNDS-1; i++)
+    for (int i = 0; i < ODOCRYPT_PBOX_SUBROUNDS-1; i++)
     {
         // Conditionally move bits between adjacent pairs of words
-        OdoCrypt_ApplyMaskedSwaps(state, perm->mask[i]);
+        Odocrypt_ApplyMaskedSwaps(state, perm->mask[i]);
         // Move the words around
-        OdoCrypt_ApplyWordShuffle(state, PBOX_M);
+        Odocrypt_ApplyWordShuffle(state, ODOCRYPT_PBOX_M);
         // Rotate the bits within words
-        OdoCrypt_ApplyPboxRotations(state, perm->rotation[i]);
+        Odocrypt_ApplyPboxRotations(state, perm->rotation[i]);
     }
-    OdoCrypt_ApplyMaskedSwaps(state, perm->mask[PBOX_SUBROUNDS-1]);
+    Odocrypt_ApplyMaskedSwaps(state, perm->mask[ODOCRYPT_PBOX_SUBROUNDS-1]);
 }
 
 void OdoCrypt_Pack(const uint64_t* state, char* bytes)
 {
     memset(bytes, 0, ODOCRYPT_DIGEST_SIZE * sizeof(char));
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
     {
         for (int j = 0; j < 8; j++)
         {
@@ -103,63 +103,63 @@ void OdoCrypt_Pack(const uint64_t* state, char* bytes)
     }
 }
 
-void OdoCrypt_ApplySboxes(uint64_t* state, size_t sz1, size_t sz2, const uint8_t sbox1[][sz1], const uint16_t sbox2[][sz2])
+void Odocrypt_ApplySboxes(uint64_t* state, size_t sz1, size_t sz2, const uint8_t sbox1[][sz1], const uint16_t sbox2[][sz2])
 {
-    const static uint64_t MASK1 = (1 << SMALL_SBOX_WIDTH) - 1;
-    const static uint64_t MASK2 = (1 << LARGE_SBOX_WIDTH) - 1;
+    const static uint64_t MASK1 = (1 << ODOCRYPT_SMALL_SBOX_WIDTH) - 1;
+    const static uint64_t MASK2 = (1 << ODOCRYPT_LARGE_SBOX_WIDTH) - 1;
     
     int smallSboxIndex = 0;
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
     {
         uint64_t next = 0;
         int pos = 0;
         int largeSboxIndex = i;
-        for (int j = 0; j < SMALL_SBOX_COUNT / STATE_SIZE; j++)
+        for (int j = 0; j < ODOCRYPT_SMALL_SBOX_COUNT / ODOCRYPT_STATE_SIZE; j++)
         {
             next |= (uint64_t) sbox1[smallSboxIndex][(state[i] >> pos) & MASK1] << pos;
-            pos += SMALL_SBOX_WIDTH;
+            pos += ODOCRYPT_SMALL_SBOX_WIDTH;
             next |= (uint64_t) sbox2[largeSboxIndex][(state[i] >> pos) & MASK2] << pos;
-            pos += LARGE_SBOX_WIDTH;
+            pos += ODOCRYPT_LARGE_SBOX_WIDTH;
             smallSboxIndex++;
         }
         state[i] = next;
     }
 }
 
-void OdoCrypt_ApplyRotations(uint64_t* state, const int* rotations)
+void Odocrypt_ApplyRotations(uint64_t* state, const int* rotations)
 {
-    uint64_t next[STATE_SIZE];
+    uint64_t next[ODOCRYPT_STATE_SIZE];
     
-    for (int i = 1; i < STATE_SIZE; i++)
+    for (int i = 1; i < ODOCRYPT_STATE_SIZE; i++)
         next[i-1] = state[i];
-    next[STATE_SIZE-1] = state[0];
+    next[ODOCRYPT_STATE_SIZE-1] = state[0];
     
-    for (int i = 0; i < STATE_SIZE; i++)
-        for (int j = 0; j < ROTATION_COUNT; j++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
+        for (int j = 0; j < ODOCRYPT_ROTATION_COUNT; j++)
         {
             next[i] ^= Rot(state[i], rotations[j]);
         }
     
-    memcpy(state, next, STATE_SIZE * sizeof(uint64_t));
+    memcpy(state, next, ODOCRYPT_STATE_SIZE * sizeof(uint64_t));
 }
 
-void OdoCrypt_ApplyRoundKey(uint64_t* state, int roundKey)
+void Odocrypt_ApplyRoundKey(uint64_t* state, int roundKey)
 {
-    for (int i = 0; i < STATE_SIZE; i++)
+    for (int i = 0; i < ODOCRYPT_STATE_SIZE; i++)
         state[i] ^= (roundKey >> i) & 1;
 }
 
-void OdoCrypt_Encrypt(OdoStruct* odo, char* cipher, const char* plain) {
-    uint64_t state[STATE_SIZE];
-    OdoCrypt_Unpack(&state[0], plain);
-    OdoCrypt_PreMix(state);
-    for (int round = 0; round < ROUNDS; round++)
+void Odocrypt_Encrypt(OdoStruct* odo, char* cipher, const char* plain) {
+    uint64_t state[ODOCRYPT_STATE_SIZE];
+    Odocrypt_Unpack(&state[0], plain);
+    Odocrypt_PreMix(state);
+    for (int round = 0; round < ODOCRYPT_ROUNDS; round++)
     {
         OdoCrypt_ApplyPbox(state, &odo->Permutation[0]);
-        OdoCrypt_ApplySboxes(state, 1 << SMALL_SBOX_WIDTH, 1 << LARGE_SBOX_WIDTH, odo->Sbox1, odo->Sbox2);
+        Odocrypt_ApplySboxes(state, 1 << ODOCRYPT_SMALL_SBOX_WIDTH, 1 << ODOCRYPT_LARGE_SBOX_WIDTH, odo->Sbox1, odo->Sbox2);
         OdoCrypt_ApplyPbox(state, &odo->Permutation[1]);
-        OdoCrypt_ApplyRotations(state, odo->Rotations);
-        OdoCrypt_ApplyRoundKey(state, odo->RoundKey[round]);
+        Odocrypt_ApplyRotations(state, odo->Rotations);
+        Odocrypt_ApplyRoundKey(state, odo->RoundKey[round]);
     }
     OdoCrypt_Pack(state, cipher);
 }
@@ -223,8 +223,6 @@ void OdoRandom_Permutation(OdoRandom* random, int* arr, size_t sz) {
     }
 }
 
-#include <stdio.h>
-
 void Odocrypt_Init(OdoStruct* odo, uint32_t key) {
     assert(odo != NULL && "OdoStruct must be initialized before calling Odocrypt_Init");
 
@@ -236,52 +234,52 @@ void Odocrypt_Init(OdoStruct* odo, uint32_t key) {
     odo->key = key;
     
     // Randomize each s-box
-    for (int i = 0; i < SMALL_SBOX_COUNT; i++)
+    for (int i = 0; i < ODOCRYPT_SMALL_SBOX_COUNT; i++)
     {
-        OdoRandom_Permutation8(&random, odo->Sbox1[i], 1 << SMALL_SBOX_WIDTH);
+        OdoRandom_Permutation8(&random, odo->Sbox1[i], 1 << ODOCRYPT_SMALL_SBOX_WIDTH);
     }
-    for (int i = 0; i < LARGE_SBOX_COUNT; i++)
+    for (int i = 0; i < ODOCRYPT_LARGE_SBOX_COUNT; i++)
     {
-        OdoRandom_Permutation16(&random, odo->Sbox2[i], 1 << LARGE_SBOX_WIDTH);
+        OdoRandom_Permutation16(&random, odo->Sbox2[i], 1 << ODOCRYPT_LARGE_SBOX_WIDTH);
     }
     
     // Randomize each p-box
     for (int i = 0; i < 2; i++)
     {
         Pbox* perm = &odo->Permutation[i];
-        for (int j = 0; j < PBOX_SUBROUNDS; j++)
-            for (int k = 0; k < STATE_SIZE/2; k++)
+        for (int j = 0; j < ODOCRYPT_PBOX_SUBROUNDS; j++)
+            for (int k = 0; k < ODOCRYPT_STATE_SIZE/2; k++)
                 perm->mask[j][k] = OdoRandom_NextLong(&random);
-        for (int j = 0; j < PBOX_SUBROUNDS-1; j++)
-            for (int k = 0; k < STATE_SIZE/2; k++)
+        for (int j = 0; j < ODOCRYPT_PBOX_SUBROUNDS-1; j++)
+            for (int k = 0; k < ODOCRYPT_STATE_SIZE/2; k++)
                 perm->rotation[j][k] = OdoRandom_Next(&random, 63) + 1;
     }
     
     // Randomize rotations
     // Rotations must be distinct, non-zero, and have odd sum
     {
-        int bits[WORD_BITS-1];
-        OdoRandom_Permutation(&random, bits, WORD_BITS-1);
+        int bits[ODOCRYPT_WORD_BITS-1];
+        OdoRandom_Permutation(&random, bits, ODOCRYPT_WORD_BITS-1);
 
         int sum = 0;
-        for (int j = 0; j < ROTATION_COUNT-1; j++)
+        for (int j = 0; j < ODOCRYPT_ROTATION_COUNT-1; j++)
         {
             odo->Rotations[j] = bits[j] + 1;
             sum += odo->Rotations[j];
         }
-        for (int j = ROTATION_COUNT-1; ; j++)
+        for (int j = ODOCRYPT_ROTATION_COUNT-1; ; j++)
         {
             if ((bits[j] + 1 + sum) % 2)
             {
-                odo->Rotations[ROTATION_COUNT-1] = bits[j] + 1;
+                odo->Rotations[ODOCRYPT_ROTATION_COUNT-1] = bits[j] + 1;
                 break;
             }
         }
     }
     
     // Randomize each round key
-    for (int i = 0; i < ROUNDS; i++) {
-        odo->RoundKey[i] = OdoRandom_Next(&random, 1 << STATE_SIZE);
+    for (int i = 0; i < ODOCRYPT_ROUNDS; i++) {
+        odo->RoundKey[i] = OdoRandom_Next(&random, 1 << ODOCRYPT_STATE_SIZE);
     }
 }
 
@@ -296,7 +294,7 @@ int Odocrypt_Hash(OdoStruct* odo, const char* pbegin, const char* pend, char* ou
     memcpy(cipher, (const void*) pbegin, len * sizeof(char));
     cipher[len] = 1;
     
-    OdoCrypt_Encrypt(odo, cipher, cipher);
+    Odocrypt_Encrypt(odo, cipher, cipher);
     KeccakP800_Permute_12rounds(cipher);
     memcpy(output, cipher, 32 * sizeof(char));
     
